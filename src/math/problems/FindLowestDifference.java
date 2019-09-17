b6c5fb57c6dbb85a3e5d1c16c7e209a4264dc6c6
Vaneetsharma11 <vaneet11webs@gmail.com>
2019-09-16 22:39:08 -0400

    Initial commit

---
+++ src/math/problems/FindLowestDifference.java
@@ -0,0 +1,30 @@
+package math.problems;
+
+public class FindLowestDifference {
+    /*
+     Implement in java.
+     Read this below two array. Find the lowest difference between the two array cell.
+     The lowest difference between cells is 1
+    */
+    static int findMinDiff(int[] arr, int n)
+    { int diff = Integer.MAX_VALUE;
+
+        for (int i=0; i<n-1; i++)
+            for (int j=i+1; j<n; j++)
+                if (Math.abs((arr[i] - arr[j]) )< diff)
+                    diff = Math.abs((arr[i] - arr[j]));
+        return diff;
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = new int[]{30, 12, 5, 9, 2, 20, 33, 1};
+        int arr2[] = new int[]{18, 3, 41, 47, 17, 36, };
+
+        System.out.println("Minimum difference is "+
+                findMinDiff(arr, arr.length));
+        System.out.println("Minimum difference is "+
+                findMinDiff(arr, arr2.length));
+    }
+
+
+}
---
+++ src/string/problems/Permutation.java
@@ -0,0 +1,32 @@
+package string.problems;
+
+public class Permutation {
+
+
+
+    /*
+     * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
+     * Write Java program to compute all Permutation of a String
+     *
+     */
+
+    static void printPermutn(String str, String ans)
+    {
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) +
+                    str.substring(i + 1);
+            printPermutn(ros, ans + ch);
+        }
+    }
+    public static void main(String[] args)
+    {
+        String s = "abb";
+        printPermutn(s, "");
+    }
+}
